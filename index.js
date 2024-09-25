52569c890f563f63c7e3ceb45f449995725537c9
Nititorn-1338-Java-labs <119284842+Rachanov@users.noreply.github.com>
2024-09-25 16:39:55 +0700

    add login&register

--- index.js
+++ index.js
@@ -10,20 +10,48 @@ mongoose.connect('mongodb+srv://Rachanov:WAQdj64jHzSNt8aX@cluster0.np6iu.mongodb
     // useNewUrlParser: true
 })
 
+global.loggedIn = null
+
 // Controller
 const indexController = require('./controllers/indexController')
 const loginController = require('./controllers/loginController')
 const registerController = require('./controllers/registerController')
+const storeUserController = require('./controllers/storeUserController')
+const loginUserController = require('./controllers/loginUserController')
+const logoutController = require('./controllers/logoutController')
+const chatbotController = require('./controllers/chatbotController')  
+const contactController = require('./controllers/contactController')  
+
+
+// middleware
+const redirectIfAuth = require('./middleware/redirectIfAuth')
 
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded())
 app.use(flash())
+app.use(expressSession({
+    secret: "node secret"
+}))
+app.use("*", (req, res, next) => {
+    loggedIn = req.session.userId
+    next()
+})
 app.set('view engine', 'ejs')
 
+
+
 app.get('/', indexController)
-app.get('/login', loginController)
-app.get('/register', registerController)
+app.get('/login', redirectIfAuth, loginController)
+app.get('/register', redirectIfAuth, registerController)
+app.post('/user/register', redirectIfAuth, storeUserController)
+app.post('/user/login', redirectIfAuth, loginUserController)
+app.get('/logout', logoutController)
+app.get('/chatbot', chatbotController)
+app.get('/contact', contactController)
+
+
+
 
 app.listen(4000, () => {
     console.log("App listening on port 4000")
